49c3c614e85935c95cd15ecb0dfea38883813ca7
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-09-23 22:11:49 +0300

    mx_memccpy

---
+++ src/mx_memccpy.c
@@ -0,0 +1,15 @@
+#include "../inc/libmx.h"
+
+void *mx_memccpy(void *restrict dst, const void *restrict src, int c, size_t n) {
+    char *p_d = dst;
+    const char *p_s = src;
+    size_t i;
+    for (i = 0; i < n; i++) {
+        *p_d = *p_s;
+        if (*p_s == c) return dst;
+        p_s++;
+        p_d++;
+    }
+    if (i == n) return NULL;
+    return dst;
+}
