7d7c630e5dc23120cb4617ff26aee2b6c0d28fed
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-09-24 18:02:15 +0300

    oopsie

--- src/mx_memrchr.c
+++ src/mx_memrchr.c
@@ -1,8 +1,8 @@
 #include "../inc/libmx.h"
 
 void *mx_memrchr(const void *s, int c, size_t n) {
-    const size_t *p_s = (size_t *)s + n;
-    const size_t delim = (size_t)c;
+    const char *p_s = s + n;
+    const char delim = c;
 
     while (n--) {
         p_s--;
