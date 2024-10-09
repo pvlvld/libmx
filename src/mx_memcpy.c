264cb02ecb65450ed9894bcff503648c08f1a025
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-10-09 03:16:01 +0300

    copy 8 bytes at once

--- src/mx_memcpy.c
+++ src/mx_memcpy.c
@@ -4,6 +4,15 @@ void *mx_memcpy(void *restrict dst, const void *restrict src, size_t n) {
     if (!src) return dst;
     char *p_d = dst;
     const char *p_s = src;
-    for (size_t i = 0; i < n; i++) *p_d++ = *p_s++;
+    size_t chunks = n / 8;
+    size_t remaining = n % 8;
+
+    while (chunks--) {
+        *(long long *)p_d = *(const long long *)p_s;
+        p_s += 8;
+        p_d += 8;
+    }
+    while (remaining--) *p_d++ = *p_s++;
+    *p_d = '\0';
     return dst;
 }
